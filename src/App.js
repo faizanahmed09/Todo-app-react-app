0fd0ddae2f577a1d8ecd856a76cbb9c71f03eb0c
zubairsattar <zubair.sattar@forwoodsafety.com>
2023-10-22 16:57:46 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useState } from "react";
+import { AiOutlineSync } from "react-icons/ai";
+import "./styles.css";
+
+export default function App() {
+  const [bgColor, setBgColor] = useState("");
+  const [errorMsg, setErrorMsg] = useState(null); // Track error messages
+
+  async function changeColor() {
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/color`);
+      
+      if (res?.data?.color) {
+        setBgColor(`#${res.data.color}`);
+        setErrorMsg(null); 
+      } else {
+        setErrorMsg("Failed to fetch color from the server.");
+      }
+    } catch (error) {
+      setErrorMsg("An error occurred while fetching the color. Please try again later.");
+    }
+  }
+
+  return (
+    <div className="app" style={{ backgroundColor: bgColor }}> 
+      <div className="mainHeading">
+        <div className="heading">
+          <h1>Color App</h1>
+          <h2>click to change color </h2>
+
+          {errorMsg && <div className="errorNotification">{errorMsg}</div>}
+
+          <hr />
+          <button className="colorChangeBtn" onClick={changeColor}>
+            <AiOutlineSync size={50}/>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
